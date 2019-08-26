54c1148416f301d01f36cd2fbb36af0b720efa4e
Pallabee Das <pallabee.das02@gmail.com>
2019-08-26 14:00:24 +0200

    added slicing and index operations

--- MatrixOperations/arrays.py
+++ MatrixOperations/arrays.py
@@ -23,3 +23,28 @@ array1 = np.array([2.7,2.2])
 array2 = np.array([3.0,1.2])
 array3 = np.hstack((array1,array2))
 print(array3)
+#One-dimensional array
+    #1.Converting list to array
+list_onedim = [5,6,7,8,9]
+array_onedim = np.array(list_onedim)
+print(array_onedim,type(array_onedim))
+    #2.Index array
+print(array_onedim[0])
+        #Use of negative index
+print(array_onedim[-1])
+        #3.Slice array
+print(array_onedim[:])
+print(array_onedim[:1])
+print(array_onedim[-1:])
+#Two-dimensional array
+    #1. Converting list to array
+list_twodim = [[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+array_twodim = np.array(list_twodim)
+print(array_twodim,type(array_twodim))
+    #2.Index array
+print(array_twodim[1,3])
+print(array_twodim[1,])
+
+        #Use of negative index
+print(array_twodim[-1,2])
+print(array_twodim[-1,2])
